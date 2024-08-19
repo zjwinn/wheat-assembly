bd42e848852b78b6747f29647bc696429a3a03af
Zachary Winn <zachary.winn@atlas-login-1.hpc.msstate.edu>
2024-08-19 12:35:18 -0500

    add updates to snakemake config for LA

--- snakemake_batch.sh
+++ snakemake_batch.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-#SBATCH --job-name="assembly_Hilliard"            #name of the job submitted
-#SBATCH -p atlas                      #name of the queue you are submitting job to
+#SBATCH --job-name="assembly_LA03136E71"          #name of the job submitted
+#SBATCH -p atlas                                  #name of the queue you are submitting job to
 #SBATCH -A gbru_wheat2
-#SBATCH -N 1                            #number of nodes in this job
-#SBATCH -n 48                           #number of cores/tasks in this job
-#SBATCH -t 7-00:00:00                                   #time allocated for this job hours:mins:seconds
-#SBATCH --mail-user=mrwillma@ncsu.edu   #email address
+#SBATCH -N 1                                      #number of nodes in this job
+#SBATCH -n 48                                     #number of cores/tasks in this job
+#SBATCH -t 7-00:00:00                             #time allocated for this job hours:mins:seconds
+#SBATCH --mail-user=zjwinn@ncsu.edu               #email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH -o "stdout.%x.%j.%N"               #standard out %x adds job name and %j adds job number to outputfile name and %N adds the node
-#SBATCH -e "stderr.%x.%j.%N"               #optional but it prints our standard error
+#SBATCH -o "stdout.%x.%j.%N"                      #standard out %x adds job name and %j adds job number to outputfile name and %N adds the node
+#SBATCH -e "stderr.%x.%j.%N"                      #optional but it prints our standard error
 
+# Module load miniconda
 module load miniconda3
 
-source activate /project/gbru_wheat2/fhb/conda/assembly_env
+# Activate the correct environment
+source activate assembly_env
 
-snakemake --cores 'all' --configfile config/Hilliard_config.yml
+# Perform the analysis with a pointer to the config file
+snakemake --cores 'all' --configfile config/LA03136E71_config.yml
